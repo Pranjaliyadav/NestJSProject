2b193e5a2d91c1f6f88ce836a3adf0d0c5db9d56
PranjaliNu10 <122367409+PranjaliNu10@users.noreply.github.com>
2023-09-20 01:39:41 +0530

    ejs and public for static code

--- index.js
+++ index.js
@@ -5,28 +5,17 @@ import path from 'path'
 
 const app = express()
 
+app.use(express.static(path.join(path.resolve(), "public"))
+)
 
-app.get("/getproducts", (req,res)=>{
-   // res.sendStatus(401) //400 - bad request, 404 -not found, 500- internal server error, 200 - ok, 401 - unauthorized
+app.set("view engine", "ejs") //need to install ejs
 
-//    res.json({
-    //created a simple API
-//     success : true,
-//     products : [],
-//    })
-
-//res.status(400).send("oops hehe") //can send something to display along with status
-
-//console.log(__dirname) //only can be used with commonJS, gives currentd directory
-
-const pathLocation = path.resolve() //gives current path
-//res.sendFile(path.join(pathLocation, "./index.html"))
-
-res.render("./index.ejs")
-
+app.get("/", (req, res) => {
+  //  res.render("index", { name: "ABhishek" })
+  res.sendFile("index")
 
 })
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log("server is working")
 })
