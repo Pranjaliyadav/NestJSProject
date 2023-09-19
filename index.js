2598ba3e4a61cb4cfbe43609ca3420565ef3baca
PranjaliNu10 <122367409+PranjaliNu10@users.noreply.github.com>
2023-09-20 00:33:48 +0530

    express

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-//onst http = require('http')
-import http from 'http'
-import randomFile from './randomfile.js'
+import express from "express";
 import fs from 'fs'
 import path from 'path'
 
-const home  = fs.readFileSync('./index.html')
 
-// console.log(path.dirname("/home/index.js"))
+const app = express()
 
-const server =  http.createServer((req, res)=>{
-    console.log("server is working")
-    
-    console.log(randomFile)
- 
-    if(req.url === "/about"){
-        res.end("<h1>About</h1>")
-    }
-    else if(req.url === "/home"){
-        res.end("home")
-    }
-    else if(req.url === "/"){
-        res.end("<h1>Dashboard</h1>")
-    }
-    else{
-        res.end("<h1>Page not found</h1>")
-    }
- 
-    
+
+app.get("/getproducts", (req,res)=>{
+   // res.sendStatus(401) //400 - bad request, 404 -not found, 500- internal server error, 200 - ok, 401 - unauthorized
+
+//    res.json({
+    //created a simple API
+//     success : true,
+//     products : [],
+//    })
+
+//res.status(400).send("oops hehe") //can send something to display along with status
+
+//console.log(__dirname) //only can be used with commonJS, gives currentd directory
+
+const pathLocation = path.resolve() //gives current path
+//res.sendFile(path.join(pathLocation, "./index.html"))
+
+res.render("./index.ejs")
+
+
 })
 
-server.listen(5000, ()=>{
-    console.log("Server is working")
+app.listen(5000, ()=>{
+    console.log("server is working")
 })
